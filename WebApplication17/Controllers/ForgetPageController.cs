e2d9c636d45518895f5beef578588e4834ee2056
tosiq ahmed <m.tosiqshafiq@yahoo.com>
2021-03-24 14:49:40 +0500

    project complted

---
+++ WebApplication17/Controllers/ForgetPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication17.Controllers
+{
+    public class ForgetPageController : Controller
+    {
+        // GET: ForgetPage
+        public ActionResult forget()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication17/Controllers/OtherPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication17.Controllers
+{
+    public class OtherPageController : Controller
+    {
+        // GET: OtherPage
+        public ActionResult other()
+        {
+            return View();
+        }
+    }
+}
